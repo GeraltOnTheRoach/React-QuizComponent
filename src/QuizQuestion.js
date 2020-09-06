126bf45afdaa30c04441a472a3009d3e0d5cb658
Geralt <antoniokin1991@gmail.com>
2020-09-06 17:31:38 +0200

    module2

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -18,12 +18,3 @@ class QuizQuestion extends Component {
 }
 
 export default QuizQuestion;
-/*
-
-9
-Displaying prop data in a paragraph tag in the QuizQuestion component
-
-Finally, in the first section's paragraph tag, display the  from this.props.
-
-Now when you run npm start and visit localhost:3000 in a web browser, you should see the text of the first question and the text of one of the answer options.
-*/
